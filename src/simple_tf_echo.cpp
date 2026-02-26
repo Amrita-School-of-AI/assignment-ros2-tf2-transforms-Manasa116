80f6808fc0a40638a7fb6eb326678c5440364d40
Manasa Chittamanasareddy <chittamanasareddy@gmail.com>
2026-02-26 11:41:46 +0530

    Final working broadcaster and echo nodes

--- src/simple_tf_echo.cpp
+++ src/simple_tf_echo.cpp
@@ -13,8 +13,8 @@ class SimpleTFEcho : public rclcpp::Node
 public:
   SimpleTFEcho()
   : Node("simple_tf_echo"),
-    buffer_(this->get_clock()),
-    listener_(buffer_)
+    tf_buffer_(this->get_clock()),
+    tf_listener_(tf_buffer_)
   {
     timer_ = this->create_wall_timer(
       1s, std::bind(&SimpleTFEcho::timer_callback, this));
@@ -23,14 +23,14 @@ public:
 private:
   void timer_callback()
   {
-    try
-    {
-      auto transform = buffer_.lookupTransform(
+    try {
+      auto transform = tf_buffer_.lookupTransform(
         "world", "robot", tf2::TimePointZero);
 
       RCLCPP_INFO(this->get_logger(),
-        "At time %.2f\n- Translation: [%.3f, %.3f, %.3f]\n- Rotation: in Quaternion [%.3f, %.3f, %.3f, %.3f]\n",
-        transform.header.stamp.sec + transform.header.stamp.nanosec * 1e-9,
+        "At time %.2f\n- Translation: [%.3f, %.3f, %.3f]\n"
+        "- Rotation: in Quaternion [%.3f, %.3f, %.3f, %.3f]\n",
+        rclcpp::Time(transform.header.stamp).seconds(),
         transform.transform.translation.x,
         transform.transform.translation.y,
         transform.transform.translation.z,
@@ -38,16 +38,15 @@ private:
         transform.transform.rotation.y,
         transform.transform.rotation.z,
         transform.transform.rotation.w);
-    }
-    catch (tf2::TransformException & ex)
-    {
-      RCLCPP_WARN(this->get_logger(), "%s", ex.what());
+
+    } catch (const tf2::TransformException & ex) {
+      RCLCPP_WARN(this->get_logger(), "Waiting for transform...");
     }
   }
 
-  tf2_ros::Buffer buffer_;
-  tf2_ros::TransformListener listener_;
   rclcpp::TimerBase::SharedPtr timer_;
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tf_listener_;
 };
 
 int main(int argc, char * argv[])
